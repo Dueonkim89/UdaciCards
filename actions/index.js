0271ceae4a865f05d09596de04d315f2175e1c2a
Dueon Kim <dueonkim89@yahoo.com>
2018-03-25 15:05:06 -0700

    Created reducer and action creator to allow adding card to deck

--- actions/index.js
+++ actions/index.js
@@ -1,12 +1,21 @@
 export const RECEIVE_DECK = 'RECEIVE_DECK';
 //export const ADD_DECK = 'ADD_DECK';
-//export const ADD_QUESTION = 'ADD_QUESTION';
+export const ADD_QUESTION = 'ADD_QUESTION';
 
 export function receiveDeck({title, questions}) {
 	return {
 		type: RECEIVE_DECK,
 		title,
 		questions
+	}
+}
+
+export function addQuestion({title, question, answer}) {
+	return {
+		type: ADD_QUESTION,
+		title,
+		question,
+		answer
 	}
 }
 
--- reducers/index.js
+++ reducers/index.js
@@ -1,7 +1,7 @@
-import { RECEIVE_DECK } from '../actions/index.js';
+import { RECEIVE_DECK, ADD_QUESTION } from '../actions/index.js';
 
 function deck(state = [], action) {
-	const { title, questions } = action;
+	const { title, questions, question, answer } = action;
 	if (action.type === RECEIVE_DECK) {
 		return [
 			...state,
@@ -9,6 +9,25 @@ function deck(state = [], action) {
 				questions
 			}
 		]
+	} else if (action.type === ADD_QUESTION) {
+		//want to preserve the order of the deck
+		let deckPosition;
+		//find the specific deck we need to add question
+		let deckToAddQuestion = [...state].filter((x, index) => {
+			if (x.title === title) {
+				deckPosition = index;
+			}
+			return x.title === title;
+		//once deck is found, map it and add on the question and answer. (Must be pure function)	
+		}).map((specficDeck) => {
+			return {...specficDeck, questions: [...specficDeck.questions, {question, answer}]}
+		})
+		//grab the remaining decks
+		let updatedDeck = [...state].filter( x => x.title !== title );
+		//add our modified deck to the exact same position that it was in
+		updatedDeck.splice(deckPosition, 0, ...deckToAddQuestion);
+		//return the updatedDeck
+		return updatedDeck;
 	} else {
 		return state;
 	}
